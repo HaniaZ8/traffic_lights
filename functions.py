bcae5cc00ac7c40f870c5ee2452e5123d3e95922
HaniaZ8 <hannazajko2004@gmail.com>
2023-02-13 14:38:53 +0100

    initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+# functions that indicate turning the lights on
+
+def red_light_on():
+    return True
+
+
+def yellow_light_on():
+    return True
+
+
+def green_light_on():
+    return True
---
+++ main.py
@@ -0,0 +1,8 @@
+# klasa, żeby włożyć tam okienko to szare/czarne, ona jest zapętlana w nieskończoność, jest odpalane przez guzik
+# guzik, żeby zapalać światła, ale po czasie wsn światła są automatyczne, tylko guzik włącza działanie mechanizmu
+# klasa z 3 metodami, które zwracają 3 różne informacje, które
+# światło się zapala, które nie i to ma się zmieniać w czasie,
+# one są w tej pętli pierwszej klasy
+# użyj funkcji sleep z pakietu time
+# interface graficzny Tkinter
+
